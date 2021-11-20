6b7819cf7c08241a921cc5c64873b171e8ca7efd
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2021-11-21 01:06:54 +0530

    encrypt and decrypt

---
+++ app/src/main/java/com/example/finacneapp/Decrypt.java
@@ -0,0 +1,59 @@
+package com.example.finacneapp;
+
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Decrypt {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static void  decrypt(){
+        try{
+            String data = "mz4jRqVxU2XOpGApk2h9Fw==";
+            String key = "1234567812345678";
+            String iv = "1234567812345678";
+
+            Base64.Decoder decoder = Base64.getDecoder();
+            byte[] encrypted1 = decoder.decode(data);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/Pkcs7Padding");
+            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
+
+            cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
+
+            byte[] original = cipher.doFinal(encrypted1);
+            String originalString = new String(original);
+            Log.d("Sr", originalString.trim());
+        }
+        catch (Exception e){
+            Log.e("Sr", e.toString());
+        }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static void encrypt(){
+        try {
+            byte[] encval=null;
+            String data = "Hello";
+            String key = "1234567812345678";
+            String iv = "1234567812345678";
+            Cipher cipher = Cipher.getInstance("AES/CBC/Pkcs7Padding");
+            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec);
+            encval=cipher.doFinal(data.getBytes());
+            String encryptedValue = Base64.getEncoder().encodeToString(encval);
+            System.out.println(encryptedValue);
+        }
+        catch (Exception e){
+            Log.e("Sr", e.toString());
+        }
+    }
+}
--- app/src/main/java/com/example/finacneapp/MainActivity.java
+++ app/src/main/java/com/example/finacneapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.finacneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println(count);
+        Decrypt decrypt = new Decrypt();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            decrypt.encrypt();
+        }
     }
 }
